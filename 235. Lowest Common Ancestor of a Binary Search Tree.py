d1c40352219659b1c70bc58684b9a80429286526
Suryakiran Sureshkumar <suryakiranbdsk@gmail.com>
2024-02-10 10:55:20 -0500

    235 new solution added

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -7,35 +7,73 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if (root == None):
-            return None
-        maxValue = max(p.val, q.val)
-        minValue = min(p.val, q.val)
-        
-        while(root):
-            if (root.val < minValue):
-                root = root.right
-            elif (root.val > maxValue):
-                root = root.left
-            else:
-                return root
-        return None
+        p_path = []
+        q_path = []
+        def findPath(root, search,path):
+            if root == None:
+                return None
+            path.append(root)
+            if root == search:
+                return True
+            if(findPath(root.left, search, path) or findPath(root.right,search,path)):
+                return True
+            path.pop(-1)
+            return False
 
-# Second solution    
+        lca = None
+        if root == None:
+            return -1
+        findPath(root, p, p_path)
+        findPath(root, q, q_path)
+        min_len = min(len(p_path),len(q_path))
+        for i in range(0,min_len):
+            if p_path[i] == q_path[i]:
+                lca = p_path[i]
+            else:
+                break
+        return lca
+
+
+
+# solution 2 
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 #         if (root == None):
 #             return None
-#         if (root.val == p.val) or (root.val == q.val):
-#             return root
+#         maxValue = max(p.val, q.val)
+#         minValue = min(p.val, q.val)
         
-#         l = self.lowestCommonAncestor(root.left, p, q)
-#         r = self.lowestCommonAncestor(root.right, p, q)
+#         while(root):
+#             if (root.val < minValue):
+#                 root = root.right
+#             elif (root.val > maxValue):
+#                 root = root.left
+#             else:
+#                 return root
+#         return None
+
+# # Second solution    
+# #         if (root == None):
+# #             return None
+# #         if (root.val == p.val) or (root.val == q.val):
+# #             return root
         
-#         if(l == None and r == None):
-#             return None
+# #         l = self.lowestCommonAncestor(root.left, p, q)
+# #         r = self.lowestCommonAncestor(root.right, p, q)
         
-#         if(l != None and r != None):
-#             return root
+# #         if(l == None and r == None):
+# #             return None
+        
+# #         if(l != None and r != None):
+# #             return root
                 
-#         if(l != None):
-#             return l
-#         return r
+# #         if(l != None):
+# #             return l
+# #         return r
