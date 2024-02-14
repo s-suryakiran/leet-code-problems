935f4ed17ae7171578cad0990ae188d3259f603a
Suryakiran Sureshkumar <suryakiranbdsk@gmail.com>
2024-02-14 09:50:58 -0500

    230 done

---
+++ 230. Kth Smallest Element in a BST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        inorder = []
+        def inordertraversal(node):
+            nonlocal inorder
+            if node == None:
+                return None
+            inordertraversal(node.left)
+            inorder.append(node.val)
+            inordertraversal(node.right)
+
+        inordertraversal(root)
+        return inorder[k-1]
+            
+        
