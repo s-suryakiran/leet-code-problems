f398d5d0013531f03e52659db537bcab40ecbbd0
Suryakiran Sureshkumar <suryakiranbdsk@gmail.com>
2024-03-24 19:11:13 -0400

    295 done

---
+++ 295. Find Median from Data Stream.py
@@ -0,0 +1,27 @@
+class MedianFinder:
+    def __init__(self):
+        self.minHeap, self.maxHeap = [], []
+        
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, -num)
+        heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+
+        if len(self.minHeap) > len(self.maxHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        
+
+    def findMedian(self) -> float:
+
+        if len(self.minHeap) == len(self.maxHeap):
+            return (-self.maxHeap[0] + self.minHeap[0])/2
+
+        else:
+            return -self.maxHeap[0]
+
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
