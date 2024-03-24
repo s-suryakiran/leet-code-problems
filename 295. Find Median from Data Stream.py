c466a5675d3cb8424d098d139186d730b76921c3
Suryakiran Sureshkumar <suryakiranbdsk@gmail.com>
2024-03-24 19:30:26 -0400

    295 done

--- 295. Find Median from Data Stream.py
+++ 295. Find Median from Data Stream.py
@@ -1,22 +1,44 @@
 class MedianFinder:
     def __init__(self):
-        self.minHeap, self.maxHeap = [], []
-        
-    def addNum(self, num: int) -> None:
-        heapq.heappush(self.maxHeap, -num)
-        heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        self.maxHeap, self.minHeap = [], []
 
-        if len(self.minHeap) > len(self.maxHeap):
+    def addNum(self, num: int) -> None:
+        if self.minHeap and num > self.minHeap[0]:
+            heapq.heappush(self.minHeap, num)
+        else:
+            heapq.heappush(self.maxHeap, -num)
+
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        if len(self.minHeap) > len(self.maxHeap) + 1:
             heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
-        
 
     def findMedian(self) -> float:
-
-        if len(self.minHeap) == len(self.maxHeap):
-            return (-self.maxHeap[0] + self.minHeap[0])/2
-
-        else:
+        if len(self.maxHeap) > len(self.minHeap):
             return -self.maxHeap[0]
+        elif len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        return (-self.maxHeap[0] + self.minHeap[0]) / 2.0
+    
+# class MedianFinder:
+#     def __init__(self):
+#         self.minHeap, self.maxHeap = [], []
+        
+#     def addNum(self, num: int) -> None:
+#         heapq.heappush(self.maxHeap, -num)
+#         heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+
+#         if len(self.minHeap) > len(self.maxHeap):
+#             heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        
+
+#     def findMedian(self) -> float:
+
+#         if len(self.minHeap) == len(self.maxHeap):
+#             return (-self.maxHeap[0] + self.minHeap[0])/2
+
+#         else:
+#             return -self.maxHeap[0]
 
         
 
@@ -25,3 +47,4 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+        
